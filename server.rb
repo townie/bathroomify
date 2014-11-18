54f24c16b8f41c9216ec600197666726496709a1
Keith Webber <kwebber@backupify.com>
2014-11-18 01:34:50 -0500

    pry terrible

--- server.rb
+++ server.rb
@@ -6,6 +6,5 @@ get '/' do
   Dotenv.load
   res = HTTParty.get("https://api.spark.io/v1/devices/#{ENV['DEVICE_ID']}/state\?access_token\=#{ENV['ACCESS_TOKEN']}")
   @status = (res["result"] == 1) ? "Open" : "Occupied"
-  binding.pry
   erb :status
 end
