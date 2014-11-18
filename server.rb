b84fd97d99f6be3e28b76e230bdfe74bfd2dbf37
Keith Webber <kwebber@backupify.com>
2014-11-18 01:26:50 -0500

    refactored out dot env files

--- server.rb
+++ server.rb
@@ -7,9 +7,8 @@ require 'dotenv'
 
 
 get '/' do
-Dotenv.load
-binding.pry
-  res = HTTParty.get("https://api.spark.io/v1/devices/#{ENV[DEVICE_ID]}/state\?access_token\=#{ENV[ACCESS_TOKEN]}")
+  Dotenv.load
+  res = HTTParty.get("https://api.spark.io/v1/devices/#{ENV['DEVICE_ID']}/state\?access_token\=#{ENV['ACCESS_TOKEN']}")
   @status = (res["result"] == 1) ? "Open" : "Occupied"
   binding.pry
   erb :status
